2c3859e8bbc91fd56487e37ad54a70fe35c3df1b
nicprkr <nicolarparker@gmail.com>
2018-02-28 12:34:17 -0800

    added both socket folders

---
+++ stickers/test.js
@@ -0,0 +1,46 @@
+const port = process.env.PORT || 10000;
+const server = require("http").Server();
+
+var io = require("socket.io")(server);
+var allqs ={};
+
+io.on("connection", function(socket){
+    
+    socket.on("joinroom", function(data){
+        console.log("joining room", data);
+        socket.join(data);
+        socket.myRoom = data;
+        
+        if(!allqs[data]){
+            allqs[data] = {
+                qobj:{}
+            };
+        }
+    });
+    
+    socket.on("answer", function(data){
+        var msg="wrong";
+        if(data == allqs[socket.myRoom].qobj.a){
+            msg ="yay";
+        }
+        socket.emit("result", msg);
+    });
+    
+    socket.on("qsubmit", function(data){
+        allqs[socket.myRoom].qobj = data;
+        socket.to(socket.myRoom).emit("newq", data);
+    });
+
+    socket.on("disconnect", function(){
+        
+    });
+    
+});
+
+server.listen(port, (err)=>{
+    if(err){
+        console.log(err);
+        return false;
+    }
+    console.log("Port is running");
+})
