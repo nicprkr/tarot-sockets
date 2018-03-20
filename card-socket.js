8e53b31ca66049e904aeba940a690dd1f6a75c0a
nicprkr <nicolarparker@gmail.com>
2018-03-20 15:30:21 -0700

    fixed merge commits

--- card-socket.js
+++ card-socket.js
@@ -2,6 +2,7 @@ const server = require("http").Server();
 const port = process.env.PORT || 10000;
 
 var io = require("socket.io")(server);
+io.origins('*:*');
 
 io.on("connection", function(socket){
     console.log("user has connected");
